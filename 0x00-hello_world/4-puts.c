b3bb8f0b770e309ff3c2e2b3ac836e9d2466a5b5
Dure Olgera <duolgera@gmail.com>
2023-06-16 15:21:28 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- * main -printing using puts fuction
+/**
+ * main -A program that printing using puts fuction
  * return:0 (we have done with success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
