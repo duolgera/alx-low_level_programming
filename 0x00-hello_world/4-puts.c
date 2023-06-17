2df7ed41647bebc24528d0423fa3a6db8b3a5baa
Dure Olgera <duolgera@gmail.com>
2023-06-17 00:30:43 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line using puts fuction
+ * main - A program that prints a line using puts function
  *
  * return: 0 (we have done with success)
  */
