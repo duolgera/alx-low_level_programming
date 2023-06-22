cc57088e9f6a9704b4a97d39573990e84c05996f
Dure Olgera <duolgera@gmail.com>
2023-06-22 15:24:28 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that multiplies two integers
+ *@c:The character to be checked
+ * Return: 1 for digit character or 0 for anytining else
+ */
+
+int _isdigit(int c)
+
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
