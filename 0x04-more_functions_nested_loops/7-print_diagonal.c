3eead079b3dbf11b8ee91434c082f31381bba066
Dure Olgera <duolgera@gmail.com>
2023-06-25 03:23:08 -0700

    print-diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,14 +10,18 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0)
+	{
+		putchar('$');
 		putchar('\n');
+	}
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < i; j++)
+		for (j = 0; j <= i; j++)
 		{
 			putchar(' ');
 		}
 	}
 	putchar('\\');
+	putchar('$');
 	putchar('\n');
 }
