1698946fa4c1a4dbb4bb4e96870331ecd8bad47c
Dure Olgera <duolgera@gmail.com>
2023-06-26 12:42:49 -0700

    Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 -pointer to updates the value it points to to 98
+ *@n:The value that is going to be updated to 98
+ * Return: Always 0 (Succsse)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
