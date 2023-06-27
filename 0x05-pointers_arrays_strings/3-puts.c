123619fb3f3e86d3171eeb279ada2312f0a36c44
Dure Olgera <duolgera@gmail.com>
2023-06-27 05:15:47 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -  a function that prints a string, followed by a new line
+ * Return: Always 0 Success
+ */
+
+int main(void)
+
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+
+	_puts(str);
+	return (0);
+}
