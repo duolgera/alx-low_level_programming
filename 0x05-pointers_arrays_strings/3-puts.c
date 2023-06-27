d3423527d0f8e523aa13595434cc164b63181293
Dure Olgera <duolgera@gmail.com>
2023-06-27 06:17:40 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,20 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main -  a function that prints a string, followed by a new line
- * Return: Always 0 Success
+ * _puts -  a function that prints a string, followed by a new line
+ * @str:- pointer to the string
+ * Return: string and new line
  */
 
-int main(void)
+void _puts(char *str)
 
 {
-	char *str;
+	int i = 0;
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-
-	_puts(str);
-	return (0);
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
