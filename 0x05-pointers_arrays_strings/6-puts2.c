7498d9dc23fd0e03a89571c0ff8d45ad6099f459
Dure Olgera <duolgera@gmail.com>
2023-06-28 01:53:57 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2- prints every other character of a string
+ * @str: a string that points
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int z = 0;
+
+	while (str[z] != '\0')
+	{
+		if (z % 2 == 0)
+		{
+			_putchar(str[z]);
+		}
+		z++;
+	}
+	_putchar('\n');
+}
