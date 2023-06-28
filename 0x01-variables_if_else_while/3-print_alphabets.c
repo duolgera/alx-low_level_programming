1fae0533f9b3ffd920c9214a2547a8d3d1256c89
Dure Olgera <duolgera@gmail.com>
2023-06-28 09:38:37 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main -program that prints lower and upercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
