5c184c8cb21fa2c3010dbd5551e426d031b2c7ea
Dure Olgera <duolgera@gmail.com>
2023-06-28 12:01:38 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit of base 10 starting 0
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
