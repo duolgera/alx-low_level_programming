751ca547a3a53aed0a8e6cca3cb8875d149ead9e
Dure Olgera <duolgera@gmail.com>
2023-07-16 11:09:00 -0700

    fixed void function bug

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,12 +4,12 @@
  * _puts_recursion- Prints a string followed by a new line.
  * @s: The string to be printed.
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
