0f3bf8ca4703c3d2fe70ef34190b33ae8a6f1b66
Dure Olgera <duolgera@gmail.com>
2023-07-18 13:43:42 -0700

    first c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,24 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Struct representing a dog
+ * @age: age of a dog
+ * @owner: owner of a dog 
+ * @name:name of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+/**
+ * int_dog -int a struct dog
+ * dog_t -typedef for struct dog
+ */
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);dog_t *new_dog(char *name, float age, char *owner);
